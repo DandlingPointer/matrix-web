2611c032a6691fc26f313dc8c8b9e502a9055914
Lennart Vogelsang <lennart@vogelsangs.net>
2014-11-17 22:37:16 +0100

    changes

--- main.js
+++ main.js
@@ -15,18 +15,18 @@ var Matrix = React.createClass({
         return {size: size, data: data};
     },
 
-    onCellChange: function(e) {
-        var matrix = this.state.data,
-            x = e.target.getAttribute('data-x'),
-            y = e.target.getAttribute('data-y');
-        matrix[x][y] = e.target.value;
-        this.setState({data: matrix});
+    onCellChangeFactory: function(x, y) {
+        return function(e) {
+            var matrix = this.state.data;
+            matrix[x][y] = e.target.value;
+            this.setState({data: matrix});
+        };
     },
-    createCell: function(x, y, val) {
+    createCell: function(x, y) {
         return (
             <td key={x+"|"+y}>
-                <input data-x={x} data-y={y} pattern="[0-9]+" class="matrix-cell" type="text"
-                    value={val} onChange={this.onCellChange} size="5">
+                <input pattern="[0-9]+" class="matrix-cell" type="text"
+                    value={this.state.data[x][y]} onChange={this.onCellChangeFactory(x, y)} size="5">
                 </input>
             </td>
         );
@@ -38,7 +38,6 @@ var Matrix = React.createClass({
             oldData = this.state.data;
 
         if (isNaN(size) || size < 1 || size > 10) { //FIXME: isNaN is potentially broken, think of something better
-            this.setState({size: size});
             return;
         }
 
@@ -55,19 +54,21 @@ var Matrix = React.createClass({
         this.setState({size: size, data: data});
     },
     render: function() {
-        var createCell = this.createCell; //FIXME: ugly way to fix the problem
-        var matrix;
-        matrix = this.state.data.map(function(row, x) {
-            var rowElements = row.map(function(cellVal, y) {
-                return createCell(x, y, cellVal);
-            });
-            console.log(rowElements);
-            return <tr key={"row"+x}> {rowElements} </tr>;
-        });
+        var matrix = [],
+            size = this.state.size;
+        for (var x = 0; x < size; x = x + 1) {
+            var elemRow = [];
+            for (var y = 0; y < size; y = y + 1) {
+                elemRow.push(this.createCell(x, y));
+            }
+            matrix.push(<tr key={"row"+x}> {elemRow} </tr>);
+        }
         return (
             <div class="matrix">
                 <form>
-                <table class="matrix-table"> <tbody> {matrix} </tbody></table>
+                <table class="matrix-table">
+                    <tbody> {matrix} </tbody>
+                </table>
                 <input type="text" title="Enter the size" pattern="[0-9]+" value={this.state.size} onChange={this.onSizeChange}/>
                 </form>
             </div>
